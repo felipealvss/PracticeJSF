9e768e11d8a6884a4aec6968cff77fddfa7261a5
Felipe Alves <lipealvessax@gmail.com>
2020-11-21 08:32:13 -0300

    Modded practice JPA

--- src/entity/Movie.java
+++ src/entity/Movie.java
@@ -12,7 +12,6 @@ public class Movie {
 	private Integer id;
 	private String name;
 	private String category;
-	//EntityManager em = JPAUtil.createEntityManager();
 	
 	//toString
 	public String toString() {
